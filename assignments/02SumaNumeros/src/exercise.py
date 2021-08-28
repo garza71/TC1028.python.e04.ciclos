9648f57f65632a6e9609ed55478c3809c0760ca2
Lina Patricia Garza Gomez <57541431+garza71@users.noreply.github.com>
2021-08-27 19:19:00 -0500

    Update exercise.py

--- assignments/02SumaNumeros/src/exercise.py
+++ assignments/02SumaNumeros/src/exercise.py
@@ -1,12 +1,6 @@
 def main():
     #escribe tu código abajo de esta línea
-    num = int(input())
-    sum = 0
-    while num != 0:
-        sum = sum + num
-        #print("Dame otro")
-        num = int(input())
-    print(sum)
+
 
 
 if __name__=='__main__':
