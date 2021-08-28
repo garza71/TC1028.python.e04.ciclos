fcaa713b0281e46410cb608635f044494246b4f1
Lina Patricia Garza Gomez <57541431+garza71@users.noreply.github.com>
2021-08-27 19:32:45 -0500

    Update input_data.py

--- assignments/07CantidadPares/tests/input_data.py
+++ assignments/07CantidadPares/tests/input_data.py
@@ -5,31 +5,31 @@ input_values = [
     # Test case 1
     (
     ["-5"],
-    ["","Total de pares=0"],
+    ["","Total de pares= 0"],
     ["No identifica que no hay números a procesar."]
     ),
     # Test case 2
     (
     ["8","2","6","-8"],
-    ["","","","", "Total de pares=3"],
+    ["","","","", "Total de pares= 3"],
     ["No cuenta correctamente la cantidad de pares"]
     ),
     # Test case 3
     (
     ["1","3","5","-1"],
-    ["","","","", "Total de pares=0"],
+    ["","","","", "Total de pares= 0"],
     ["No cuenta correctamente la cantidad de pares"]
     ),
     # Test case 4
     (
     ["8","1","9","7","-9"],
-    ["","","","","", "Total de pares=1"],
+    ["","","","","", "Total de pares= 1"],
     ["No cuenta correctamente la cantidad de pares"]
     ),
     # Test case 4
     (
     ["4","0","1","3","-2"],
-    ["","","","","", "Total de pares=2"],
+    ["","","","","", "Total de pares= 2"],
     ["No cuenta correctamente la cantidad de pares"]
     )
     ]
